826d32ac3d0cf4eef208347012995658ac5040a2
MirzaSuljic <mirza.suljic@gmail.com>
2020-11-29 18:35:49 +0100

    initial commit

---
+++ src/components/Directory/Directory.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ShopMen from '../../assets/shopMens.jpg';
+import ShopWomen from '../../assets/shopWomens.jpg';
+import './Directory.css';
+
+const Directory = (props) => {
+    return (
+        <div className="directory">
+            <div className="wrap">
+                <div
+                className="item"
+                style={{
+                    backgroundImage:`url(${ShopWomen})`
+                }}>
+                    <a>
+                        Shop Womens
+                    </a>
+                </div>
+                <div
+                className="item"
+                style={{
+                    backgroundImage:`url(${ShopMen})`
+                }}>
+                    <a>
+                    Shops Mens
+                    </a>
+
+                </div>
+            </div>
+            
+        </div>
+    )
+};
+
+export default Directory;
---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Homepage.css';
+import Directory from '../../components/Directory/Directory';
+
+const Homepage = () => {
+    return (
+        <section className="homepage">
+            <Directory/>
+        </section>
+    )
+};
+
+export default Homepage;
